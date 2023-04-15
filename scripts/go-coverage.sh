d832304c286384dcdff0cc3a71c16979fd80a067
Dimas Adiyaksa <dimas.adiyaksa@privyid.tech>
2023-04-15 23:41:28 +0700

    add go coverage script

---
+++ scripts/go-coverage.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e -o pipefail
+
+REQUIRED=$1
+COVERAGE=$(go test -cover ./... | grep 'coverage: ' | sed -e 's/.*coverage:\(.*\)% of.*/\1/')
+
+if [[ "${COVERAGE%.*}" -lt "$REQUIRED" ]]; then
+    echo "Coverage is $COVERAGE%, but $REQUIRED% is required"
+    exit 1
+fi
